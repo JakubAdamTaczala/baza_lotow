4c6e7f7ad0279da5cbcec7bc506996ca4beb8413
JakubAdamTaczala <37074367+JakubAdamTaczala@users.noreply.github.com>
2018-05-03 19:34:19 +0200

    Add files via upload

--- php/user_s_account.php
+++ php/user_s_account.php
@@ -31,8 +31,59 @@
     echo '<p>Witaj '.$_SESSION['imie'].' '.$_SESSION['nazwisko'].' [<a href="logout.php">Wyloguj się!</a>]<p/>';
     echo '<p>E-mail: '.$_SESSION['mail'].'<p/>';
     echo '<p>Tele.: '.$_SESSION['telefon'].'<p/>';
-    echo "to jest panel user";
 
 ?>
+
+<?php
+    ini_set("display_errors", 0);
+    require_once 'connect.php';
+    $polaczenie = mysqli_connect($host, $db_user, $db_password);
+    mysqli_query($polaczenie, "SET CHARSET utf8");
+    mysqli_query($polaczenie, "SET NAMES 'utf8' COLLATE 'utf8_polish_ci'");
+    mysqli_select_db($polaczenie, $db_name);
+
+    $rezultat=@$polaczenie->query("SELECT ID_REZERWACJI, REZERWACJE.ID_LOTU, SKAD, DATA_ODLOTU, GODZINA_ODLOTU, DOKAD, DATA_PRZYLOTU, CZAS_PRZYLOTU, ILOSC_MIEJSC, STATUS, Uwagi FROM REZERWACJE, LOTY WHERE LOTY.ID_LOTU=REZERWACJE.ID_LOTU AND ID_KLIENTA=1 ORDER BY `REZERWACJE`.`ID_REZERWACJI` DESC");
+    $ile=$rezultat->num_rows;
+
+echo<<<END
+
+    <h2>Historia rezerwacji</h2>
+    <table border="1" cellpadding="10" cellspacing="1">
+    <tr>
+    <td>Nr rezerwacji</td> <td>Nr lotu</td> <td>Miejsce startu</td> <td>Data startu</td> <td>Godzina startu</td> <td>Miejsce lądowania</td> <td>Data lądowania</td> <td>Godzina lądowania</td> <td>Liczba miejsc</td> <td>Status</td> <td>Uwagi</td> <td>Anuluj rezerwację</td>
+    </tr>
+END;
+
+    for ($i = 1; $i <= $ile; $i++)
+    {
+        $row = mysqli_fetch_assoc($rezultat);
+        $rezerwacja = $row['ID_REZERWACJI'];
+        $id = $row['ID_LOTU'];
+        $miejsce_startu = $row['SKAD'];
+        $data_startu = $row['DATA_ODLOTU'];
+        $godzina_startu = $row['GODZINA_ODLOTU'];
+        $miejsce_ladowania = $row['DOKAD'];
+        $data_ladowania = $row['DATA_PRZYLOTU'];
+        $godzina_ladowania  = $row['CZAS_PRZYLOTU'];
+        $ilosc_miejsc = $row['ILOSC_MIEJSC'];
+        $status = $row['STATUS'];
+        $uwagi= $row['Uwagi'];
+
+echo<<<END
+    <tr>
+    <td>$rezerwacja</td> <td>$id</td> <td>$miejsce_startu</td> <td>$data_startu</td> <td>$godzina_startu</td> <td>$miejsce_ladowania</td> <td>$data_ladowania</td> <td>$godzina_ladowania</td> <td>$ilosc_miejsc</td> <td>$status</td> <td>$uwagi</td> <td><a href="anuluj.php">Anuluj</a></td>
+    </tr>
+END;
+    }
+
+echo<<<END
+    </table>
+END;
+
+    $polaczenie->close();
+?>
+
+    <br/>[<a href="rezerwacja.php">Rezerwacja</a>]
+
 </body>
 </html>
